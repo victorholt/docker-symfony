678a3c5466292a1765002faa31617950bd65aa06
Victor Holt <vholt@nerdery.com>
2017-11-20 12:32:26 -0600

    nginx on prod will, by default, run on port 80 instead of port 8080

--- bin/build-prod.sh
+++ bin/build-prod.sh
@@ -17,4 +17,4 @@ docker run -d -p 3306:3306 --name $appname_mysql $appname_mysql_img
 
 docker run -d -p 9000:9000 --link $appname_mysql:db --link $appname_mail --name $appname $appname_img
 
-docker run -d -p 8080:80 --link $appname:php --name $appname_nginx $appname_nginx_img
+docker run -d -p 80:80 --link $appname:php --name $appname_nginx $appname_nginx_img
